b4bdb33aa84589a24cd13ffff144b1f78c27eff2
PetrPix <ppix50@gmail.com>
2025-12-05 20:52:30 +0100

    Initial commit

---
+++ posts.ts
@@ -0,0 +1,20 @@
+import { Post } from './types';
+
+export const posts: Post[] = [
+  {
+    "id": "vzorovy-clanek",
+    "slug": "yzomandias-kral-sceny-se-vraci",
+    "title": "YZOMANDIAS: KRÁL SCÉNY SE VRACÍ",
+    "excerpt": "Hlava Milion+ se vrací s novým projektem, který mění pravidla hry. Exkluzivní pohled do zákulisí příprav.",
+    "author": "Redakce",
+    "date": "2024-05-20",
+    "category": "RAPPERS",
+    "tags": [
+      "MILION+",
+      "YZO",
+      "NOVINKA"
+    ],
+    "imageUrl": "https://picsum.photos/seed/yzo/800/600",
+    "content": "## Král je zpět\n\nYzomandias, vlastním jménem Jakub Vlček, znovu dokazuje, proč je na vrcholu potravního řetězce české rapové scény. Jeho nejnovější projekt není jen album, je to manifest.\n\n## Nový zvuk\n\nProdukce se ujali dlouholetí spolupracovníci, ale slyšíme i nové vlivy drillu a elektroniky.\n\n* \"Melanž\" - absolutní banger\n* \"Rodina\" - osobní zpověď\n\n> \"Nedělám rap pro fame, dělám to pro kulturu.\"\n\nOčekávání jsou obrovská."
+  }
+];
